b29dced34f409ba98c816c31a0c3e6f890dd462a
MoesaMalik <malikmoesa@gmail.com>
2023-09-17 16:58:05 -0400

    print statement

---
+++ src/AddressBook.java
@@ -0,0 +1,15 @@
+
+
+import java.util.LinkedList;
+public class AddressBook {
+
+
+    public static void main(String[] args){
+            System.out.println("Address Book");
+    }
+    public AddressBook(){};
+    public void addBuddy(){};
+
+    public void removeBuddy(){};
+
+}
