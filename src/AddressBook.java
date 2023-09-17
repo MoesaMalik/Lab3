d1b982cdcc2b76720ac12e57311097bca3735355
MoesaMalik <malikmoesa@gmail.com>
2023-09-17 18:10:54 -0400

    Completed functions for AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,14 +1,24 @@
-
+import java.util.LinkedList;
 
 public class AddressBook {
 
+    private LinkedList<BuddyInfo> addressBook;
 
     public static void main(String[] args){
-            System.out.println("Address Book");
+            BuddyInfo buddy = new BuddyInfo("Tom", "Carleton","613");
+            AddressBook addressBook = new AddressBook();
+            addressBook.addBuddy(buddy);
+            addressBook.removeBuddy(buddy);
     }
-    public AddressBook(){};
-    public void addBuddy(){};
+    public AddressBook(){
+        addressBook = new LinkedList<>();
+    }
+    public void addBuddy(BuddyInfo buddy){
+        addressBook.add(buddy);
+    }
 
-    public void removeBuddy(){};
+    public void removeBuddy(BuddyInfo buddy){
+        addressBook.remove(buddy);
+    }
 
 }
